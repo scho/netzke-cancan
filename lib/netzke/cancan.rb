73f95024f1c9d6d6543155027301a0e75cbd3bc5
Georg Meyer <georgmeyer83@googlemail.com>
2012-08-05 02:33:59 +0200

    Fix format

--- lib/netzke/cancan.rb
+++ lib/netzke/cancan.rb
@@ -1,19 +1,21 @@
 module Netzke
   class Base
 
-    # This method is overwritten in respect to current_user in netzke components
+    # This method is returns the current ability with the current user from netzke
     #
     # @return [Ability]
     def current_ability
       @current_ability ||= Ability.new(Netzke::Core.current_user)
     end
 
-     # cancan calls helper_method, which is present in any controller, but not in a netzke components
-     #
-     # @params *args
-     def self.helper_method(*args)
-     end
+    # cancan calls helper_method, which is present in any controller, but not in a netzke components
+    #
+    # @params *args
+    def self.helper_method(*args)
+      # We don't do anything since there is no view layer in netzke components
+    end
 
-     include ::CanCan::ControllerAdditions
+    # Include the cancan controller additions
+    include ::CanCan::ControllerAdditions
   end
 end
