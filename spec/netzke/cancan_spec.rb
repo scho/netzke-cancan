c5258036b6a2e4b84402044775f14911497005c5
Georg Meyer <georgmeyer83@googlemail.com>
2012-08-05 02:32:43 +0200

    Fix format

--- spec/netzke/cancan_spec.rb
+++ spec/netzke/cancan_spec.rb
@@ -17,13 +17,12 @@ describe Netzke::CanCan do
   end
   
   describe "#current_ability" do
-	it "should respond_to current_ability" do
-	  @component.should respond_to(:current_ability)
-	end
+	  it "should respond_to current_ability" do
+	    @component.should respond_to(:current_ability)
+	  end
 
-	it "should return instance of Ability" do
-	  @component.current_ability.should be_a(Ability)
-	end
+	  it "should return instance of Ability" do
+	    @component.current_ability.should be_a(Ability)
+	  end
   end
-
 end
